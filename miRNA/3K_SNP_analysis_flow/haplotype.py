27f6ba980a3086d465bfde8dcbb7ae3def44f38b
longknown <longknown@gmail.com>
2015-09-10 12:42:25 +0800

    2015/9/10 morning stage1 haplotype analysis

--- miRNA/3K_SNP_analysis_flow/haplotype.py
+++ miRNA/3K_SNP_analysis_flow/haplotype.py
@@ -2,10 +2,30 @@
 import sys
 __author__ = 'thomas'
 '''Usage
-        Description： This is the 1st step of the haplotype analysis, currently you've got basic info of SNPs at hand;
-        :param a file containing miRNA and its SNPs (even NULL values are accepted, although will be eliminated later)
-        :returns -m for miRNA haplotype; -h for haplotype pattern
+        :param inputs a simple file with miRNA name & SNP id
+        :return haplotype for each miRNA
 '''
 
 infile = sys.argv[1]
+if len(sys.argv) == 2:
+    fw = sys.stdout
+else:
+    outf = sys.argv[2]
+    fw = open(outf, 'w')
+mir_snp = {}
+with open(infile, 'r') as f:
+    for line in f:
+        line = line.rstrip('\n')
+        elements = line.split()
+        mirname = elements[0]
+        snp = elements[1]
+        if mirname not in mir_snp:
+            mir_snp[mirname] = [snp]
+        else:
+            mir_snp[mirname].append(snp)
 
+out_result = ''
+for name in mir_snp:
+    out_result += name+'\t'+str(len(mir_snp[name]))+'\t'+'\t'.join(mir_snp[name])+'\n'
+fw.write(out_result)
+fw.close()
